0410e876576ecf2b7fd78b11a5c14e0e8e268740
Juan Pablo Ospina Gutierrez <101611634+jupospinagu@users.noreply.github.com>
2022-03-31 00:20:17 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -11,7 +11,11 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 """
-
+import csv
+with open("data.csv", "r") as file:
+ data = file.readlines()
+ data = [line.replace("\n", "") for line in data]
+ data = [line.split("\t") for line in data]
 
 def pregunta_01():
     """
@@ -21,15 +25,7 @@ def pregunta_01():
     214
 
     """
-import csv
-with open("data.csv", "r") as file:
- data = file.readlines()
- data = [line.replace("\n", "") for line in data]
- data = [line.split("\t") for line in data]
-
 data_columna2 = [fila[1] for fila in data]
-
-
 data_columna2= [
     [int(field) if i_row >= 0 else field for field in row]
     for i_row, row in enumerate(data_columna2)
